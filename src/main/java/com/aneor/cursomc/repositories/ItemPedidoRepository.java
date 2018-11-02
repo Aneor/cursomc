7254acc6540dd1672588efbd5e4d18210a1d9b87
Aneor Junior <aneorjunior@gmail.com>
2018-11-02 17:39:23 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/aneor/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.aneor.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aneor.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
