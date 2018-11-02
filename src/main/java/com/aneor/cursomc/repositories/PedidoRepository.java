863c5dc9f7310ccd484ef49c55caf43b3f5f781c
Aneor Junior <aneorjunior@gmail.com>
2018-11-02 17:10:31 -0300

    Pedido, Pagamento, EstadoPagamento

---
+++ src/main/java/com/aneor/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.aneor.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aneor.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
