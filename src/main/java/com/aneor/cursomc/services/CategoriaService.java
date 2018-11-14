b04772e4b1155a1906ebdab62ea2d00e2666e9da
Aneor Junior <aneorjunior@gmail.com>
2018-11-13 22:54:22 -0200

    Validação sintatica com Bean Validation

--- src/main/java/com/aneor/cursomc/services/CategoriaService.java
+++ src/main/java/com/aneor/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.aneor.cursomc.domain.Categoria;
+import com.aneor.cursomc.dto.CategoriaDTO;
 import com.aneor.cursomc.repositories.CategoriaRepository;
 import com.aneor.cursomc.services.exceptions.DataIntegrityException;
 import com.aneor.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
